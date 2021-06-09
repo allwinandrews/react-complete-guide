8b797efca32ca66598ff9cb7eb07994fe735d56d
Andrews <allwinandrews@gmail.com>
2021-06-10 00:44:48 +0530

    Adds Expenses Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,13 @@
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
